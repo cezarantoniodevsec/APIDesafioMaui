a14d10c5a329a37ba5de633c4cf3d897b39c702d
cezar <cezarantoniodev@gmail.com>
2023-10-27 10:59:06 -0300

    Adding Automapper

---
+++ Helpers/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+using System;
+using WebApi.Entities;
+using WebApi.Models.Product;
+using AutoMapper;
+
+namespace ProductsAPI.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<CreateProductRequest, Product>();
+
+            CreateMap<UpdateProductRequest, Product>();
+
+        }
+    }
+}

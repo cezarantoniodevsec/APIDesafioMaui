8bb76c5f1b1da51d8318c08fd22092992c2fda67
Cezar Antonio de Sousa <trc6019558@unimedbh.com.br>
2023-10-30 13:46:55 -0300

    Changes in DataContext

--- Helpers/DataContext.cs
+++ Helpers/DataContext.cs
@@ -1,63 +1,14 @@
-using System.Data;
-using Dapper;
-using Microsoft.Extensions.Options;
-using Npgsql;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
 
 namespace ProductsAPI.Helpers
 {
-    public class DataContext
+    public class DataContext: DbContext
     {
-        private DbSettings _dbSettings;
-
-        public DataContext(IOptions<DbSettings> dbSettings)
+        public DbSet<Product> Products { get;set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            _dbSettings = dbSettings.Value;
-        }
-
-        public IDbConnection CreateConnection()
-        {
-            var connectionString = $"";
-            return new NpgsqlConnection(connectionString);
-        }
-
-        public async Task Init()
-        {
-            await _initDatabase();
-            await _initTables();
-        }
-
-        private async Task _initDatabase()
-        {
-            var connectionString = $";";
-            using var connection = new NpgsqlConnection(connectionString);
-            var sqlDbCount = $"SELECT COUNT(*) FROM pg_database WHERE datname = 'produtos_uui7';";
-            var dbCount = await connection.ExecuteScalarAsync<int>(sqlDbCount);
-            if (dbCount == 0)
-            {
-                var sql = $"CREATE DATABASE \"produtos_uui7\"";
-                await connection.ExecuteAsync(sql);
-            }
-        }
-
-        private async Task _initTables()
-        {
-            // create tables if they don't exist
-            using var connection = CreateConnection();
-            await _initUsers();
-
-            async Task _initUsers()
-            {
-                var sql = """
-                CREATE TABLE IF NOT EXISTS Products(
-                    Id Serial primary key, 
-                    Nome varchar(50),
-                    Descricao varchar(20), 
-                    Preco Numeric(11,2),
-                    ImagemUrl varchar(50)
-                );
-            """;
-                await connection.ExecuteAsync(sql);
-            }
+            optionsBuilder.UseSqlite(@"DataSource=products.db;Cache=Shared");
         }
     }
 }
